208735bf2effecdc0a7eca95da1ec6576209114b
wangzishuo <wangzishuo@growingio.com>
2020-06-11 10:43:37 +0800

    deploy test

--- home_application/views.py
+++ home_application/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def hello(request):
+    return HttpResponse('Hello World!')
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
 # 装饰器引入 from blueapps.account.decorators import login_exempt
